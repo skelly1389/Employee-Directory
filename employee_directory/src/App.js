a79d8d24ec335b8887931e7f37ce10d564519021
Sean Kelly <invertedrdm@gmail.com>
2021-05-05 20:05:39 -0400

    think the sort functions work, need to add event handlers

--- employee_directory/src/App.js
+++ employee_directory/src/App.js
@@ -6,7 +6,29 @@ import API from "./utils/API";
 
 class App extends Component {
   state = {
-    employees: []
+    employees: [],
+  };
+
+  sortByNameAsc = () => {
+    let sortedEmpsAsc;
+    sortedEmpsAsc = this.state.employees.sort((a, b) => {
+      return a.name.last - b.name.last;
+    });
+
+    this.setState({
+      employees: sortedEmpsAsc,
+    });
+  };
+
+  sortByNameDsc = () => {
+    let sortedEmpsDsc;
+    sortedEmpsDsc = this.state.employees.sort((a, b) => {
+      return b.name.last - a.name.last;
+    });
+
+    this.setState({
+      employees: sortedEmpsDsc,
+    });
   };
 
   componentDidMount() {
@@ -15,19 +37,24 @@ class App extends Component {
 
   getEmps = () => {
     API.getRandomEmp()
-      .then(res =>
+      .then((res) =>
         this.setState({
-          employees: res.data.results
+          employees: res.data.results,
         })
       )
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   };
 
   render() {
     return (
       <Wrapper>
         <Title>Employee List</Title>
-        {this.state.employees.map(employee => (
+        <select>
+          <option value="albums">Last Name Ascending</option>
+          <option value="members">Last Name Descending</option>
+          <option value="formed">Lives Outside USA</option>
+        </select>
+        {this.state.employees.map((employee) => (
           <EmpCard
             key={employee.login.uuid}
             name={employee.name.last + ", " + employee.name.first}
